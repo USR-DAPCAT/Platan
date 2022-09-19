42aa9d610c6fe8223bbe6fb98d0cc7e96fc6bf6f
Ramon Puig Treserres <raypuig@gmail.com>
2022-09-19 17:01:27 +0200

    act

---
+++ R/dt_diagnostics.R
@@ -0,0 +1,8 @@
+#' Prices of 50,000 round cut diamonds
+#' @format A data frame with 25 rows and 3 variables
+#' \describe{
+#' \item{idp}{idp (1--5)}
+#' \item{cod}{cod (E11   I10   I25 I50.9 )}
+#' \item{dat}{dat (20080115--20080215)}
+#' }
+"dt_diagnostics"
---
+++ data-raw/dt_diagnostics.R
@@ -0,0 +1,3 @@
+## code to prepare `dt_diagnostics` dataset goes here
+
+usethis::use_data(dt_diagnostics, overwrite = TRUE)
