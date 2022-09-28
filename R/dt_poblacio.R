f90e5dd91cd851fa4088618428742ccff4176dbf
Ramon Puig Treserres <raypuig@gmail.com>
2022-09-28 16:45:09 +0200

    act

---
+++ R/dt_poblacio.R
@@ -0,0 +1,11 @@
+#' Bases de dades de Poblacio
+#' @format Una data frame amb 5 files i 6 variables
+#' \describe{
+#' \item{idp}{idp (1--5)}
+#' \item{sexe}{sexe (D H)}
+#' \item{dnaix}{dnaix (19200801--19400101 )}
+#' \item{situacio}{situacio (A D T)}
+#' \item{entrada}{entrada (20060101--20060101)}
+#' \item{sortida}{sortida (20090102--20181231)}
+#' }
+"dt_poblacio"
---
+++ data-raw/dt_poblacio.R
@@ -0,0 +1,3 @@
+## code to prepare `dt_poblacio` dataset goes here
+
+usethis::use_data(dt_poblacio, overwrite = TRUE)
