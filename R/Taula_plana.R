4f41d5682aa3da34457f1e9c71a62b0fdf483b94
RayPuig <raypuig@gmail.com>
2022-10-17 07:54:31 +0200

    act

---
+++ R/Taula_plana.R
@@ -0,0 +1,11 @@
+#' Bases de dades de Poblacio
+#' @format Una data frame amb 30 files i 194 variables
+#' \describe{
+#' \item{idp}{idp (1--30)}
+#' \item{dtindex}{dtindex ()}
+#' \item{DG.ALCOHOL}{DG.ALCOHOL ()}
+#' \item{DG.ARTPER}{DG.ARTPER ()}
+#' \item{DG.AVC}{DG.AVC ()}
+#' \item{DG.DM_ALTRES}{DG.DM_ALTRES ()}
+#' }
+"Taula_plana"
